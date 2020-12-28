d8bf3486f579235312ffb34e5197e80163a8a2b5
sc052133 <sai.prasad@cerner.com>
2020-12-28 22:58:47 +0530

    basic test addded

--- test/controllers/urlparser_controller_test.rb
+++ test/controllers/urlparser_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UrlparserControllerTest < ActionDispatch::IntegrationTest
+
   test "should get index" do
     get urlparser_index_url
     assert_response :success
@@ -8,6 +9,21 @@ class UrlparserControllerTest < ActionDispatch::IntegrationTest
 
   test "should get show" do
     get urlparser_show_url
+    assert_response :success
+  end
+
+  test "should alert when URL is empty" do
+    get urlparser_index_url, params: { url: "" }
+    assert_equal "URL cant be empty", flash[:alert]
+  end
+
+  test "should alert when URL is bad" do
+    get urlparser_index_url, params: { url: "abc" }
+    assert_equal "URL invalid", flash[:alert]
+  end
+
+  test "should get data when URL is valid" do
+    get urlparser_index_url, params: { url: "https://www.314e.com/", parent: 1 }
     assert_response :success
   end
 
